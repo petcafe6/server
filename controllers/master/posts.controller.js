e4cb8e97e342a478aa9743ae9eb2e01cfc754646
Ali TEK <alitek@gmail.com>
2025-01-05 15:08:13 +0300

    comment system ok

--- controllers/master/posts.controller.js
+++ controllers/master/posts.controller.js
@@ -2,7 +2,9 @@ module.exports = (dbModel, sessionDoc, req) => new Promise(async (resolve, rejec
 	try {
 		switch (req.method) {
 			case 'GET':
-				if (req.params.param1) {
+				if (req.params.param2 == 'comments' && req.params.param1) {
+					getComments(dbModel, sessionDoc, req).then(resolve).catch(reject)
+				} else if (req.params.param1) {
 					getOne(dbModel, sessionDoc, req).then(resolve).catch(reject)
 				} else {
 					getList(dbModel, sessionDoc, req).then(resolve).catch(reject)
@@ -10,8 +12,12 @@ module.exports = (dbModel, sessionDoc, req) => new Promise(async (resolve, rejec
 				break
 
 			case 'POST':
-				if (req.params.param1 == 'like' && req.params.param2) {
+				if (req.params.param2 == 'like' && req.params.param1) {
 					like(dbModel, sessionDoc, req).then(resolve).catch(reject)
+				} else if (req.params.param2 == 'addNewComment' && req.params.param1) {
+					addNewComment(dbModel, sessionDoc, req).then(resolve).catch(reject)
+				} else {
+					return reject(`parameters incorrect`)
 				}
 
 				break
@@ -25,11 +31,69 @@ module.exports = (dbModel, sessionDoc, req) => new Promise(async (resolve, rejec
 	}
 })
 
+function getComments(dbModel, sessionDoc, req) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let options = {
+				page: req.query.page || 1,
+				limit: req.query.pageSize || 50,
+				sort: { _id: -1 },
+				populate: [{
+					path: 'commentBy',
+					select: '_id name username role profilePicture location'
+				}]
+			}
+			let filter = {
+				post: req.params.param1
+			}
+			dbModel.posts_comments.paginate(filter, options)
+				.then(async result => {
+
+					resolve(result)
+				})
+				.catch(reject)
+
+		} catch (err) {
+			reject(err)
+		}
+	})
+}
+
+
+function addNewComment(dbModel, sessionDoc, req) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			// if (!req.params.param2) return reject(`param2 required`)
+			let postDoc = await dbModel.posts.findOne({ _id: req.params.param1 })
+			if (!postDoc) return reject(`post not found`)
+			let comment = (req.getValue('text') || '').trim()
+			if (!comment) return reject(`text required`)
+
+			const newCommentDoc = new dbModel.posts_comments({
+				post: postDoc._id,
+				commentBy: sessionDoc.user,
+				text: comment,
+			})
+			newCommentDoc.save()
+				.then(async newCommentDoc => {
+					postDoc.commentCount++
+					await postDoc.save()
+					resolve(newCommentDoc)
+				})
+				.catch(reject)
+
+		} catch (err) {
+			reject(err)
+		}
+	})
+}
+
+
 function like(dbModel, sessionDoc, req) {
 	return new Promise(async (resolve, reject) => {
 		try {
-			if (!req.params.param2) return reject(`param1 required`)
-			let doc = await dbModel.posts.findOne({ _id: req.params.param2 })
+			// if (!req.params.param2) return reject(`param2 required`)
+			let doc = await dbModel.posts.findOne({ _id: req.params.param1 })
 			if (!doc) return reject(`post not found`)
 			let oldStatus = false
 			if (await dbModel.posts_likes.countDocuments({ post: doc._id, likedBy: sessionDoc.user }) > 0) {
